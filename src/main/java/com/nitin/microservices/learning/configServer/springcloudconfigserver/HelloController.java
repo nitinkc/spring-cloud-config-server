68435451a9e7fcaa202820d5c0ac8dc1ab52620e
Nitin Chaurasia <gs.nitin@gmail.com>
2019-11-17 17:44:43 -0500

    Create HelloController.java

---
+++ src/main/java/com/nitin/microservices/learning/configServer/springcloudconfigserver/HelloController.java
@@ -0,0 +1,14 @@
+package com.nitin.microservices.learning.configServer.springcloudconfigserver;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello : Spring Boot is Up and Running";
+		
+	}
+}
